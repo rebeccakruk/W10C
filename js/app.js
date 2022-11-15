fc002101899ac37433f2217dec6353947b186324
rk <rebeccakruk@outlook.com>
2022-11-14 23:21:30 -0700

    JavaScript Events Complete

--- js/app.js
+++ js/app.js
@@ -1,48 +1,12 @@
 document.body.style.backgroundColor = `lightgrey`;
 
-// function spaceBar(event){
-//     // document.body.style.backgroundImage = `(src="https://www.datocms-assets.com/75076/1656656339-macgregor_tartan_2.jpeg")`;
-//     if (key === "" || code == "space" || keyCode == 32){
-//     document.body.style.backgroundColor = `limegreen`;
-// }
-// }
-// document.body.onkeydown = function(event) {
-//     if (event.key ==" " ||
-//     event.code == "Space" ||
-//     event.keyCode == 32) {
-//         document.body.style.backgroundImage =  `(src="https://www.datocms-assets.com/75076/1656656339-macgregor_tartan_2.jpeg")`;
-//  }
-// }
-// document.body.addEventListener(`onkeydown`, spaceBar);
-//     if(code === `space`){
-//         console.log(`Space pressed`);
-//     }
-
-// function spaceBar() {
-//     document.getElementById(`spacebar`).style.backgroundImage = `(src="https://www.datocms-assets.com/75076/1656656339-macgregor_tartan_2.jpeg")`;
-// }
-
-// window.onkeydown = function (event) {
-//     if (event.which == 32) {
-//         document.body.backgroundColor = `black`;
-//     }
-// }
-
-// addEventListener(`keydown`, (event) => {});
-// onkeydown = (event) => {};
-
-function goBold(event){
+function goBold(event) {
     event.target.style.color = `limegreen`;
     event.target.style.fontSize = `3rem`;
 }
 
-   window.onkeypress = function(event) {
-      if (event.which == 32) {
-        document.body.style.backgroundColor = `hotpink`; 
-      }
-    }
 
-function goBig(event){
+function goBig(event) {
     event.target.innerHTML += ` HOME OF TERRIBLE PUNS!`;
     head.style.fontStyle = `italic`;
     head.style.color = `dodgerblue`;
@@ -55,15 +19,21 @@ firstP.addEventListener(`mouseover`, (goBold));
 let head = document.getElementById(`head`);
 head.addEventListener(`click`, (goBig));
 
-// function changeColor(i){
-//     let body = document.getElementById(`background`);
-//     let color = [`blue`, `purple`, `pink`, `limegreen`];
-//     for(i=0; i<color.length; i++){
-//         document.style.backgroundColor = color(i);
-//     }
-// }
+window.onkeypress = function (event) {
+    if (event.which == 32) {
+        document.body.style.color = `hotpink`;
+        document.body.style.fontSize = `30px`;
+    }
+}
 
-// let changeColor = document.getElementById(`spacebar`);
-// setTimeout(function(changeColor)){spaceBar.value = `blue`}, 15000);
-// // 
+colors = [`red`, `darkgrey`, `rebeccapurple`, `darkgoldenrod`, `aqua`, `bisque`]
+colorIndex =0
+function changeColor(){
+    document.body.style.backgroundColor = colors[colorIndex]
+    colorIndex = (colorIndex +1) % colors.length
+}
 
+function startChange(){
+    setInterval(`changeColor()`, 15*1000)
+}
+window.onload = startChange
